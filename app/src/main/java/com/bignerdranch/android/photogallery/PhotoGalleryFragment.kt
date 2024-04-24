f91b54d97c7d7fd28b1966fe9e0d28af60e5220e
EkatTk <ektk1805@gmail.com>
2024-04-24 14:06:50 +0700

    Сохранение результата поиска при закрытии приложения

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -83,8 +83,8 @@ class PhotoGalleryFragment : Fragment() {
         inflater.inflate(R.menu.fragment_photo_gallery, menu)
         val searchItem: MenuItem = menu.findItem(R.id.menu_item_search)
         val searchView = searchItem.actionView as SearchView
-        searchView.apply { setOnQueryTextListener(object :
-            SearchView.OnQueryTextListener {
+        searchView.apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(queryText: String): Boolean {
                 Log.d(TAG, "QueryTextSubmit: $queryText")
                 photoGalleryViewModel.fetchPhotos(queryText)
@@ -95,6 +95,18 @@ class PhotoGalleryFragment : Fragment() {
                 return false
             }
         })
+            setOnSearchClickListener {
+                searchView.setQuery(photoGalleryViewModel.searchTerm, false)
+            }
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_clear -> {
+                photoGalleryViewModel.fetchPhotos("")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
 
     }
